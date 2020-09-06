3262798f712fc1169b21365d855056e36a082592
nick ristagno <3mpirical10510@gmail.com>
2020-09-06 15:53:34 -0600

    initial commit (directories refactored and babel module resolver added)

---
+++ config-overrides.js
@@ -0,0 +1,25 @@
+const { override, addBabelPlugin } = require('customize-cra');
+
+// These aliases allow us to import directly from the given directories.
+// !!! Make sure to add these aliases to the jsconfig.json for autocompletion.
+// !!! And make sure to add them to the .eslintrc.json so eslint can resolve the path.
+module.exports = override(
+  addBabelPlugin([
+    'module-resolver', {
+      root: ['.'],
+      alias: {
+        '@assets': './src/assets',
+        '@components': './src/components',
+        '@context': './src/context',
+        '@elements': './src/elements',
+        '@forms': './src/forms',
+        '@hooks': './src/hooks',
+        '@screens': './src/screens',
+        '@styles': './src/styles',
+        '@utils': './src/utils',
+        '@tests': './src/tests',
+        '@serviceWorkers': './src/serviceWorkers',
+      },
+    },
+  ]),
+);
